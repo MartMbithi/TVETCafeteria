339fa89bb923b827498c2027cde3d6e175f69001
Mart Mbithi <martdevelopers254@gmail.com>
2024-03-17 14:30:44 +0300

    Implement Create and Update Operations On Products

--- TvetCafeteriaSystem/app.py
+++ TvetCafeteriaSystem/app.py
@@ -465,9 +465,39 @@ def create_product():
     return jsonify({'message': 'Product created successfully!'}), 201
 
 #Get List Of Products
+@app.route('/api/products', methods=['GET'])
+@jwt_required()
+def get_products():
+    products = Product.query.all()
+    results = [
+        {
+            "product_id": product.product_id,
+            "location_id": product.location_id,
+            "product_name": product.product_name,
+            "name": product.name,
+            "price": product.price,
+            "category": product.category
+        } for product in products]
+
+    return jsonify(results), 200
+
 
 #Update Product
+@app.route('/api/products/<int:product_id>', methods=['PUT'])
+@jwt_required()
+def update_product(product_id):
+    data = request.json
+    product = Product.query.get_or_404(product_id)
 
+    product.location_id = data.get('location_id')
+    product.product_name = data.get('product_name')
+    product.name = data.get('name')
+    product.price = data.get('price')
+    product.category = data.get('category')
+
+    db.session.commit()
+
+    return jsonify({'message': 'Product updated successfully!'}), 200
 
 #Delete Product
 
