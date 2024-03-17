d74cb73513cc29d1c68567542ed3532e739c305b
Mart Mbithi <martdevelopers254@gmail.com>
2024-03-17 11:48:48 +0300

    Implement Bookshop and Cafeteria Model

--- TvetCafeteriaSystem/app.py
+++ TvetCafeteriaSystem/app.py
@@ -12,24 +12,27 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
+
 # Database models
-class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
+# Bookshop Model
+class Bookshop(db.Model):
+    bookshop_id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.String(100))
+    bookshop_name = db.Column(db.String(100))
+    
+# Cafeteria Model
+class Cafeteria(db.Model):
+    cafeteria_id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.String(100))
+    cafeteria_name = db.Column(db.String(100))
+# Digital Center Model
+# Locations Model
+# Orders Model
+# Pharmacy Model
+# Product Model
+# Tuck_shop Model
+# Users Model
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Numeric(10, 2), nullable=False)
-    category = db.Column(db.String(50), nullable=False)
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
 
 # Routes
 @app.route('/api/login', methods=['POST'])
@@ -41,7 +44,10 @@ def login():
     user = Users.query.filter_by(username=username, password=password).first()
     if user:
         access_token = create_access_token(identity=user.user_id)
-        return jsonify({'access_token': access_token}), 200
+        return jsonify(
+            {'message': 'Login successfull',
+                'access_token': access_token}
+            ), 200
     else:
         return jsonify({'message': 'Invalid username or password'}), 401
 
